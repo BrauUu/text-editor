52ddf9cb8f9fbbc046b9654fcc108916dcc19afc
Brayandotexe <brayanfrank.sm@gmail.com>
2020-06-06 11:32:06 -0300

    commmit

--- editor.js
+++ editor.js
@@ -1,5 +1,11 @@
 editor.document.designMode="on"
 
+function changeFontStyle(){
+    let select = document.getElementById('font-select')
+    var value = select.options[select.selectedIndex].value;
+    editor.document.execCommand('fontName', false, value)
+}
+
 function toBold(){
     editor.document.execCommand('bold',false,null)
 }
@@ -12,12 +18,32 @@ function toUnderline(){
     editor.document.execCommand('underline',false,null)
 }
 
-function undo(){
-    editor.document.execCommand('undo',false,null)
+function toParagraph(){
+    editor.document.execCommand('indent', false, null)
 }
 
-function redo(){
-    editor.document.execCommand('redo',false,null)
+function toSuperscript(){
+    editor.document.execCommand('superscript', false, null)
+}
+
+function toSubscript(){
+    editor.document.execCommand('subscript', false, null)
+}
+
+function alignCenter(){
+    editor.document.execCommand('justifyCenter', false, null)
+}
+
+function alignLeft(){
+    editor.document.execCommand('justifyLeft', false, null)
+}
+
+function alignRight(){
+    editor.document.execCommand('justifyRight', false, null)
+}
+
+function justify(){
+    editor.document.execCommand('justifyFull', false, null)
 }
 
 function toOrderedList(){
@@ -26,4 +52,18 @@ function toOrderedList(){
 
 function toUnorderedList(){
     editor.document.execCommand('insertUnorderedList', false, null)
+}
+
+function changeSize(){
+    let select = document.getElementById('font-size-select')
+    var value = select.options[select.selectedIndex].value;
+    editor.document.execCommand('fontSize', false, value)
+}
+
+function undo(){
+    editor.document.execCommand('undo',false,null)
+}
+
+function redo(){
+    editor.document.execCommand('redo',false,null)
 }
